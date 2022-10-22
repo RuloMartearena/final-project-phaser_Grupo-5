413d4651879aa045bb58e09379ee9a270e83b8e5
Axel Robles <axel.xxlirio@gmail.com>
2022-10-22 20:06:55 -0300

    Add: new recordatorios.RoblesAxel

---
+++ trabajo-final-phaser_grupo-5/src/components/Recordatorio.js
@@ -0,0 +1,18 @@
+//import '../Css/recordatorio.css';
+
+function Recordatorio() {
+    return (
+        <section>
+            <div className="div__recordatorio-conteiner">
+                <h2 className="h2__title"><u>Recordatorios</u></h2>
+                <ul className="ul__recordatorio">
+                    <li>El personaje tiene que llegar a la cima</li>
+                    <li>No debes caer</li>
+                    <li>Supera todas las plataformas para obtener el premio</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Recordatorio;
